e4852479748fcc95cd24a6aec28b2476975c1dec
latishasumardy <sumardy5031@mydusd.org>
2018-03-01 19:53:42 -0800

    fix nav bar

--- Recipe_Tracker/app/src/main/java/com/example/latisha/recipetracker/Home.java
+++ Recipe_Tracker/app/src/main/java/com/example/latisha/recipetracker/Home.java
@@ -100,17 +100,15 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_add_recipe) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_breakfast) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_lunch) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_dinner) {
 
         }
 
