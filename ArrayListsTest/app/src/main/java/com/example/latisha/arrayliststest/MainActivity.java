e2e89423eac8173fea019c7771a452886e514b7e
latishasumardy <sumardy5031@mydusd.org>
2018-03-06 10:45:48 -0800

    Add Other inputs

--- ArrayListsTest/app/src/main/java/com/example/latisha/arrayliststest/MainActivity.java
+++ ArrayListsTest/app/src/main/java/com/example/latisha/arrayliststest/MainActivity.java
@@ -19,22 +19,28 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
     Button save;
     ArrayList<String> addArray = new ArrayList<String>();
-    EditText txt;
+    EditText name;
     ListView show;
+    EditText ingredients;
+    EditText recipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt = (EditText)findViewById(R.id.Name);
+        name = (EditText)findViewById(R.id.Name);
+        ingredients = (EditText)findViewById(R.id.ingredients);
+        recipe = (EditText)findViewById(R.id.instructions);
         show = (ListView)findViewById(R.id.listView);
         save = (Button)findViewById(R.id.button3);
         save.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                String getInput = txt.getText().toString();
+                String getInput = name.getText().toString();
+                String getIng = ingredients.getText().toString();
+                String getRecipe = recipe.getText().toString();
 
                 if(addArray.contains(getInput)){
                     Toast.makeText(getBaseContext(), "Item already added to Array", Toast.LENGTH_LONG);
@@ -47,6 +53,33 @@ public class MainActivity extends Activity {
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, addArray);
                     show.setAdapter(adapter);
                     ((EditText)findViewById(R.id.Name)).setText(" ");
+
+                }
+
+                if(addArray.contains(getIng)){
+                    Toast.makeText(getBaseContext(), "Item already added to Array", Toast.LENGTH_LONG);
+                }
+                else if (getIng == null || getIng.trim().equals("")){
+                    Toast.makeText(getBaseContext(),"Input Field is Empty",Toast.LENGTH_LONG);
+                }
+                else {
+                    addArray.add(getIng);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, addArray);
+                    show.setAdapter(adapter);
+                    ((EditText)findViewById(R.id.ingredients)).setText(" ");
+
+                }
+                if(addArray.contains(getRecipe)){
+                    Toast.makeText(getBaseContext(), "Item already added to Array", Toast.LENGTH_LONG);
+                }
+                else if (getRecipe == null || getRecipe.trim().equals("")){
+                    Toast.makeText(getBaseContext(),"Input Field is Empty",Toast.LENGTH_LONG);
+                }
+                else {
+                    addArray.add(getRecipe);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, addArray);
+                    show.setAdapter(adapter);
+                    ((EditText)findViewById(R.id.instructions)).setText(" ");
 
                 }
             }
